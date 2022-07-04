5f00a050bc0c8bac16ed9350eeba8e8229e3e506
Alvaro De la Rica <alvaro.delarica.alvarez@gmail.com>
2022-07-04 23:19:18 +0200

    improve homepage showing  a few tweets

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
+import prisma from 'lib/prisma'
+import { getTweets } from 'lib/data.js'
+import Tweets from 'components/Tweets'
 
-export default function Home() {
+export default function Index({ tweets }) {
   const { data: session, status } = useSession()
   const router = useRouter()
 
@@ -13,5 +16,29 @@ export default function Home() {
     router.push('/home')
   }
 
-  return <a href='/api/auth/signin'>login</a>
+  return (
+    <div className='mt-10'>
+      <Tweets tweets={tweets} />
+      <div className='text-center p-4 border m-4'>
+        <h2 className='mb-10'>Join the conversation!</h2>
+        <a
+          className='border px-8 py-2 mt-5 font-bold rounded-full color-accent-contrast bg-color-accent hover:bg-color-accent-hover-darker'
+          href='/api/auth/signin'
+        >
+          login
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export async function getServerSideProps() {
+  let tweets = await getTweets(prisma, 3)
+  tweets = JSON.parse(JSON.stringify(tweets))
+
+  return {
+    props: {
+      tweets,
+    },
+  }
 }
